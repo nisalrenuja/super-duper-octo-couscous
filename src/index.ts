680853e19bac79d8cad1c37d507ac738d063a3dc
Nisal Renuja Palliyaguru <42801983+nisalrenuja@users.noreply.github.com>
2022-02-22 14:56:05 +0530

    Middleware added

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
 import express, { Application } from "express";
+import morgan from "morgan";
 
 const PORT = process.env.PORT || 8000;
 
 const app: Application = express();
 
+app.use(express.json());
+app.use(morgan("tiny"));
+app.use(express.static("public"));
 app.get("/ping", async (_req, res) => {
   res.send({
     message: "hello",
